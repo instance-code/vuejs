29becbb0a58a1928b6d86a90a4d7099b2275c343
Tan Nguyen <tannq@fabbi.io>
2021-11-05 11:42:35 +0700

    add laravel+vuejs

---
+++ laravel/resources/src/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import { createApp } from 'vue';
+import App from '@/App.vue';
+
+const app = createApp(App);
+const plugin = {
+  install () {
+    // app.config.globalProperties.$axios = axios;
+  }
+};
+
+// [store, i18n, router, plugin].forEach((i) => {
+//   app.use(i);
+// });
+
+app.mount("#app");
+
