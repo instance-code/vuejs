366e235866018f54c12e880a23f0d5f132c7c66c
NMDung21 <dungbgbg67@gmail.com>
2021-11-10 11:05:12 +0700

    manhdungcommit

--- vuecli/src/App.vue
+++ vuecli/src/App.vue
@@ -1,46 +1,22 @@
 <template>
-  <ul>
-    <li v-for="task in tasks">
-      <a href="#"  @click.prevent="clickTask(task)">{{ task.name }}</a>
-    </li>
-  </ul>
-  <div v-if="!Object.keys(activeTask).length">
-    Không có giá trị nào được chọn!
-  </div>
-  <div v-else class="d-flex justify-between">
-    <span>{{ activeTask.name }}</span>
-    <span>{{ activeTask.status ? "Hoàn Thành" : "Chưa hoàn thành" }}</span>
+  <div>
+    <calculator></calculator>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      tasks: [
-        { id: 1, name: "Laravel", status: false },
-        { id: 2, name: "Vuejs", status: true },
-        { id: 3, name: "Bootstrap", status: false },
-      ],
-      activeTask: {}
-    }
-  },
+import Calculator from '@/components/Calculator.vue'
 
-  methods: {
-    clickTask(task) {
-      this.activeTask = task;
-    }
+export default {
+  name : 'App',
+  components: {
+    Calculator
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background: #31475e;
+  }
 </style>
