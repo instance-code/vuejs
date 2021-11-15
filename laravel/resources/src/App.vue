0ca176b51a438314650e78cc1d4472dcccdfb3a8
unknown <you@example.com>
2021-11-15 14:57:01 +0700

    abcdxyz

--- laravel/resources/src/App.vue
+++ laravel/resources/src/App.vue
@@ -1,36 +1,37 @@
 <template>
-  <ul>
-    <li v-for="task in tasks">
-      <a href="#"  @click.prevent="clickTask(task)">{{ task.name }}</a>
-    </li>
-  </ul>
-  <div v-if="!Object.keys(activeTask).length">
-    Không có giá trị nào được chọn!
-  </div>
-  <div v-else class="d-flex justify-between">
-    <span>{{ activeTask.name }}</span>
-    <span>{{ activeTask.status ? "Hoàn Thành" : "Chưa hoàn thành" }}</span>
-  </div>
+    <div v-for="param in params" :key="param.id">
+        <span>{{ param.id }}</span>
+        <input v-model="param.first" placeholder="Nhập tên họ" />
+        <input v-model="param.last" placeholder="Nhập tên" />
+        <button v-on:click="deleteItemParam(item)">Remove</button>
+    </div>
+    <button @click="SaveParam">Save</button>
+    <pre>{{ $data }}</pre>
+    <button @click="AddParam">Add</button>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      tasks: [
-        { id: 1, name: "Laravel", status: false },
-        { id: 2, name: "Vuejs", status: true },
-        { id: 3, name: "Bootstrap", status: false },
-      ],
-      activeTask: {}
-    }
-  },
-
-  methods: {
-    clickTask(task) {
-      this.activeTask = task;
-    }
-  }
-}
+    data() {
+        return {
+            params: [{ id: 1, first: "", last: "" }],
+        };
+    },
+    methods: {
+        AddParam: function () {
+            this.params.push({
+                id: this.params.length + 1,
+                first: "",
+                last: "",
+            });
+        },
+        deleteItemParam: function (item) {
+            let index = this.params.indexOf(item);
+            this.params.splice(index, 1);
+        },
+        SaveParam(){
+            console.log(this.params);
+        }
+    },
+};
 </script>
-
--- laravel/resources/src/app.js
+++ laravel/resources/src/app.js
@@ -1,18 +1,8 @@
-'use strict';
+"use strict";
 
-import { createApp } from 'vue';
-import App from '@/App.vue';
+import { createApp } from "vue";
+import App from "@/App.vue";
 
 const app = createApp(App);
-const plugin = {
-  install () {
-    // app.config.globalProperties.$axios = axios;
-  }
-};
-
-// [store, i18n, router, plugin].forEach((i) => {
-//   app.use(i);
-// });
 
 app.mount("#app");
-
