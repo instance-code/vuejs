6dc645b540659fbffd3d9808a046b626895c0fff
tiep <nguyenviettiepdn@gmail.com>
2021-11-18 08:51:02 -0800

    sua index

--- laravel/resources/views/blog/index.blade.php
+++ laravel/resources/views/blog/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-@include('layouts.nav')
+    @include('layouts.nav')
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col">
@@ -16,65 +16,79 @@
                         <table class="table">
                             <thead>
                                 <tr>
-                                    <th scope="col">
+                                    <th>
+
+
                                         <div class="checkAll">
                                             <label class="label">
-                                      <input class="js-check-all" type="checkbox" name="" data-check-all="language">
-                                      <span class="text">Check All</span>
-                                    </label>
+                                                <input class="js-check-all" type="checkbox" name=""
+                                                    data-check-all="language">
+                                            </label>
                                         </div>
-                                    </th>
 
-                                    <th scope="col">id</th>
-                                 
-                                    <th scope="col">{{__('blogedit.blog_title')}}</th>
-                                    <th scope="col">{{__('blogedit.blog_content')}}</th>
-                                    <th scope="col">{{__('blogedit.created_by')}}</th>
-                                    <th scope="col">{{__('blogedit.updated_by')}}</th>
-                                    <th scope="col">{{__('blogedit.updated_date')}}</th>
-                                    <th scope="col">#</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($blogs as $key => $blog)
-                                    <tr>
-                                        <th class="item"> 
-                                            <label class="label">
-                                      <input type="checkbox" name="language" value="language1">
-                                    </label>
-                                        </li></th>
-                                        <th scope="row">{{ $key }}</th>
-                                        <td>{{ $blog->blog_title}}</td>
-                                        <td>{{ $blog->blog_content }}</td>
-                                        <td>{{ $blog->created_by }}</td>
-                                        <td>{{ $blog->updated_by }}</td>
-                                        <td>{{ $blog->updated_at }}</td>
-                                        <td>
-                                            <form action="{{ route('blog.destroy', [$blog->id]) }}" method="post">
-                                                @method('DELETE')
-                                                @csrf
-                                                <button onclick="return confirm('Bạn muốn xoá danh mục truyện này không');"
-                                                    class="btn btn-danger">{{__('blogedit.delete')}}</button>
-                                            </form>
-                                        </td>
-                                        <td>
-                                            <a href="{{ route('blog.edit', [$blog->id]) }}"
-                                                class="btn btn-primary">{{__('blogedit.edit')}}</a>
-                                            <form action="" method="post">
-                                                @method('PUT')
-                                                @csrf
 
-                                            </form>
-                                        </td>
-                                    </tr>
 
-                                @endforeach
-                            </tbody>
-                        </table>
 
                     </div>
+                    </th>
+
+                    <th scope="col">id</th>
+
+                    <th scope="col">{{ __('blogedit.blog_title') }}</th>
+                    <th scope="col">{{ __('blogedit.blog_content') }}</th>
+                    <th scope="col">{{ __('blogedit.created_by') }}</th>
+                    <th scope="col">{{ __('blogedit.updated_by') }}</th>
+                    <th scope="col">{{ __('blogedit.updated_date') }}</th>
+                    <th scope="col">#</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($blogs as $key => $blog)
+                            <tr>
+                                <th>
+
+                                    <ul class="js-check-all-target list" style="list-style: none" data-check-all="language">
+                                        <li class="item">
+                                            <label class="label">
+                                                <input type="checkbox" name="language" value="language1">
+                                                <span class="text"></span>
+                                            </label>
+                                        </li>
+
+                                    </ul>
+                                </th>
+                                <th scope="row">{{ $key }}</th>
+                                <td>{{ $blog->blog_title }}</td>
+                                <td>{{ $blog->blog_content }}</td>
+                                <td>{{ $blog->created_by }}</td>
+                                <td>{{ $blog->updated_by }}</td>
+                                <td>{{ $blog->updated_at }}</td>
+                                <td>
+                                    <form action="{{ route('blog.destroy', [$blog->id]) }}" method="post">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button onclick="return confirm('Bạn muốn xoá danh mục truyện này không');"
+                                            class="btn btn-danger">{{ __('blogedit.delete') }}</button>
+                                    </form>
+                                </td>
+                                <td>
+                                    <a href="{{ route('blog.edit', [$blog->id]) }}"
+                                        class="btn btn-primary">{{ __('blogedit.edit') }}</a>
+                                    <form action="" method="post">
+                                        @method('PUT')
+                                        @csrf
+
+                                    </form>
+                                </td>
+                            </tr>
+
+                        @endforeach
+                    </tbody>
+                    </table>
+
                 </div>
             </div>
         </div>
+    </div>
     </div>
 @endsection
--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -10,19 +10,21 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+
 </head>
 <body>
     <div id="appp">
-       
-
+      
         <main class="py-4">
             @yield('content')
         </main>
@@ -30,25 +32,25 @@
 </body>
 </html>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.js"></script>
-<script src="">
+<script>
     $(function() {
-        $('.js-check-all').on('change', function(event) {
-            var group = $(event.target).data('check-all');
-            var dataCheckGroup = '[data-check-all="' + group + '"]';
+    $('.js-check-all').on('change', function(event) {
+        var group = $(event.target).data('check-all');
+        var dataCheckGroup = '[data-check-all="' + group + '"]';
 
-            if ($(event.target).prop('checked')) {
-                $('.js-check-all-target')
-                    .filter(dataCheckGroup)
-                    .find('input[type="checkbox"]:not(:disabled)')
-                    .prop('checked', true);
-
-                return;
-            }
-
+        if ($(event.target).prop('checked')) {
             $('.js-check-all-target')
                 .filter(dataCheckGroup)
                 .find('input[type="checkbox"]:not(:disabled)')
-                .prop('checked', false);
-        });
+                .prop('checked', true);
+
+            return;
+        }
+
+        $('.js-check-all-target')
+            .filter(dataCheckGroup)
+            .find('input[type="checkbox"]:not(:disabled)')
+            .prop('checked', false);
     });
+});
 </script>
