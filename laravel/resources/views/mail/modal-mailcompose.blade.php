713612e5e54012a95453eeb51c88e551e3b08929
Thanh Tung <tungdore77@gmail.com>
2021-12-29 23:58:29 +0700

    view modal table

--- laravel/resources/views/mail/modal-mailcompose.blade.php
+++ laravel/resources/views/mail/modal-mailcompose.blade.php
@@ -25,18 +25,18 @@
                         </div>
                     </div>
 
-                    <div class="row modal-textbox" >
+                    <div class="row modal-textbox">
                         <div class="col-2">
                             哑元文本:
                         </div>
-                        <div class="col-4" >
-                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <div class="col-4">
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                         </div>
                         <div class="col-2">
                             哑元文本:
                         </div>
-                        <div class="col-4" >
-                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <div class="col-4">
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                         </div>
                     </div>
 
@@ -44,13 +44,13 @@
                         <div class="col-2">
                             哑元文本:
                         </div>
-                        <div class="col-4" >
+                        <div class="col-4">
                             <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                         </div>
                         <div class="col-2">
                             哑元文本:
                         </div>
-                        <div class="col-4" >
+                        <div class="col-4">
                             <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                         </div>
                     </div>
@@ -63,7 +63,76 @@
                             <button type="button" class="btn btn-primary">的荒谬</button>
                         </div>
                     </div>
-
+                    <div class="row modal-textbox">
+                        <div class="col-2">
+                            哑元文本
+                        </div>
+                    </div>
+                    <div class="table-wrapper-scroll-y my-custom-scrollbar">
+                        <table class="table table-bordered table-striped mb-0 table-mail ">
+                            <thead class="table-head ">
+                                <tr>
+                                    <th scope="col" class="col-table-head">
+                                        <span class="justify-content-md-center">
+                                            使用的
+                                        </span> 
+                                        <button type="button" class="btn btn-primary button-plus">+</button>
+                                    </th>
+                                    <th scope="col" class="col-table-head">这本书</th>
+                                    <th scope="col" class="col-table-head">非典型
+                                        <button type="button" class="btn btn-primary button-plus">+</button>
+                                    </th>
+                                    <th scope="col" class="col-table-head last">生成出
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="overfollow-body">
+                                <tr>
+                                    <td>Mark
+                                        <button type="button" class="btn btn-primary button-plus">+</button>
+                                    </td>
+                                    <td>Otto
+                                        <button type="button" class="btn btn-primary button-plus">+</button>
+                                    </td>
+                                    <td>@mdo
+                                        <button type="button" class="btn btn-primary button-plus">+</button>
+                                    </td>
+                                    <td>                                  
+                                        <button type="button" class="btn btn-primary">的荒谬</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Jacob
+                                        <button type="button" class="btn btn-primary button-plus">+</button>
+                                    </td>
+                                    <td>Thornton
+                                        <button type="button" class="btn btn-primary button-plus">+</button>
+                                    </td>
+                                    <td>@fat
+                                        <button type="button" class="btn btn-primary button-plus">+</button>
+                                    </td>
+                                    <td>
+                                        <button type="button" class="btn btn-primary">的荒谬</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Jacob
+                                        <button type="button" class="btn btn-primary button-plus">+</button>
+                                    </td>
+                                    <td>Thornton
+                                        <button type="button" class="btn btn-primary button-plus">+</button>
+                                    </td>
+                                    <td>@fat
+                                        <button type="button" class="btn btn-primary button-plus">+</button>
+                                    </td>
+                                    <td>
+                                        <button type="button" class="btn btn-primary">的荒谬</button>
+                                    </td>
+                                </tr>
+                                
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
